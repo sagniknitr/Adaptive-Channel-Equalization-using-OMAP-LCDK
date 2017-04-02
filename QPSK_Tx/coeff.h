352662d6693fafd3eb03b5abb64d96d4f9af3535
Sagnik Basu <113ec0199@nitrkl.ac.in>
2017-04-02 07:03:12 -0700

    Add files via upload

---
+++ QPSK_Tx/coeff.h
@@ -0,0 +1,9 @@
+/* coeff.h ... sqrt-root raised-cosine      */
+/* DF2 filter coefficients                  */
+/* exported from MATLAB using fir_dump2c.m  */
+
+
+#define B_SIZE 121
+
+extern float B[];
+
